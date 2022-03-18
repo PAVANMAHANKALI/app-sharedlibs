7aaf6fb9895b964c71d8ce3dbbbedb2b30e1aefe
PAVANMAHANKALI <32824156+PAVANMAHANKALI@users.noreply.github.com>
2022-03-19 00:38:18 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,10 +1,9 @@
 def call(ip,user,credId){
   sshagent([credId]) {
-    ips.each {
-      echo "deploying on ${it}"
+    
       sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat/apache-tomcat-9.0.56/webapps/azure.war"
       sh "ssh ${user}@${ip} /opt/tomcat/apache-tomcat-9.0.56/bin/shutdown.sh"
       sh "ssh ${user}@${ip} /opt/tomcat/apache-tomcat-9.0.56/bin/startup.sh"
-    }
+    
    }
 }
